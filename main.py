ec582a0790b2df285ac6dc0808469f1d5b4591a7
Nikita <xisoniki@gmail.com>
2024-12-22 21:05:46 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('7944304752:AAEGLjXQI9LiySM7F4AkwG4bUSHKtq9Kbu0')
-
-@bot.message_handler(commands=['start'])
-def main(message):
-    bot.send_message(message.id, 'Hi!')
-
-bot.polling(non_stop=True)
